ab068dbcbb56009f7f218d7be0419f8e152dd054
Ankush Gupta <ankushgupta@Ankushs-MacBook-Air.local>
2025-10-08 02:50:31 +0530

    cookie name changed

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ app.use(
 
 app.use(
   session({
-    name: "session",
+    name: "session-tiqueon-cookie",
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
     secure: config.NODE_ENV == 'production',
