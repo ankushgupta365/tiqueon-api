f767c699519d288c6236c4e5d0bd8fd40c10478a
Ankush Gupta <ankushgupta@Ankushs-MacBook-Air.local>
2025-10-08 01:29:47 +0530

    serializeUser and deserializeUser reverted back and sameSite set to none"
    "

--- src/config/passport.config.ts
+++ src/config/passport.config.ts
@@ -62,47 +62,5 @@ passport.use(
   )
 );
 
-// passport.serializeUser((user: any, done) => done(null, user));
-
-
-interface SessionUser {
-  _id: string;
-  name: string;
-  email: string;
-  profilePicture?: string | null;
-  isActive: boolean;
-  lastLogin: Date | null;
-  createdAt: Date;
-  updatedAt: Date;
-  password?: string;
-  currentWorkspaceId?: string;
-}
-
-
-passport.serializeUser((user: any, done) => {
-  try {
-
-    const userObject = user.toObject ? user.toObject() : user;
-
-
-    const sessionUser: SessionUser = {
-      _id: userObject._id.toString(), 
-      name: userObject.name,
-      email: userObject.email,
-      profilePicture: userObject.profilePicture,
-      isActive: userObject.isActive,
-      lastLogin: userObject.lastLogin,
-      createdAt: userObject.createdAt,
-      updatedAt: userObject.updatedAt,
-      password: userObject.password ? userObject.password : null,
-      currentWorkspaceId: userObject.currentWorkspace ? userObject.currentWorkspace.toString() : undefined,
-    };
-
-    done(null, sessionUser);
-  } catch (error) {
-    console.error("Serialization Error:", error);
-    done(error, false);
-  }
-});
-
+passport.serializeUser((user: any, done) => done(null, user));
 passport.deserializeUser((user: any, done) => done(null, user));
